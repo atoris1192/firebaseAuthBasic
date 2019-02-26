b3c02e3cf9a94e4fe908845a63c80d82f0be76db
atoris1192 <atoris1192@gmail.com>
2019-02-26 10:20:50 +0900

    firest commit

---
+++ src/main.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+  'use strict' 
+
+  const 
+
+  const firebase = require('firebase/app')
+  require('firebase/auth')
+  const config = {
+    apiKey: "AIzaSyCbwkLe6ZFPLMxAoT7iNjSKEFf6hsCuXZA",
+    authDomain: "test-store-b68ee.firebaseapp.com",
+  }
+  firebase.initializeApp(config)
+
+
+}) 
